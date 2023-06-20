54abe43b233d218da22da04cda38014ce7d45c2a
chuxin <2670957513@qq.com>
2023-06-20 19:03:04 +0800

    bubble v1.0.0

--- main.go
+++ main.go
@@ -1,31 +1,105 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
+var DB *gorm.DB
+
+type Todo struct {
+	ID     int    `json:"id"`
+	Title  string `json:"title"`
+	Status bool   `json:"status"`
+}
+
+func initMysql() (err error) {
+	dsn := "root:chuxin0816@tcp(127.0.0.1:3306)/bubble?charset=utf8mb4&parseTime=True&loc=Local"
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
 func main() {
+	//连接数据库
+	err := initMysql()
+	if err != nil {
+		panic(err)
+	}
+	//模型绑定
+	DB.AutoMigrate(&Todo{})
 	r := gin.Default()
-	r.Static("/xxx", "./statics")
 	r.LoadHTMLGlob("templates/*")
-	r.GET("/index.html", func(c *gin.Context) {
+	r.Static("/static", "static")
+	r.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.html", nil)
 	})
-	r.GET("/about.html", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "about.html", nil)
-	})
-	r.GET("/blog.html", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "blog.html", nil)
-	})
-	r.GET("/client.html", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "client.html", nil)
-	})
-	r.GET("/contact.html",func(c *gin.Context){
-		c.HTML(http.StatusOK,"contact.html",nil)
-	})
-	r.GET("/services.html",func(c *gin.Context){
-		c.HTML(http.StatusOK,"services.html",nil)
-	})
+
+	//v1
+	v1Group := r.Group("v1")
+	{
+		//待办事项
+		//添加
+		v1Group.POST("/todo", func(c *gin.Context) {
+			//1.从请求中把数据拿出来
+			var todo Todo
+			c.BindJSON(&todo)
+			//2.存入数据库
+			//3.返回响应
+			if err = DB.Create(&todo).Error; err != nil {
+				c.JSON(http.StatusOK, gin.H{"error": err.Error()})
+			} else {
+				c.JSON(http.StatusOK, todo)
+			}
+		})
+		//查看所有待办事项
+		v1Group.GET("/todo", func(c *gin.Context) {
+			var todoList []Todo
+			if err = DB.Find(&todoList).Error; err != nil {
+				c.JSON(http.StatusOK, gin.H{"error": err.Error()})
+			} else {
+				c.JSON(http.StatusOK, todoList)
+			}
+		})
+		//修改某一待办事项
+		v1Group.PUT("/todo/:id", func(c *gin.Context) {
+			id, ok := c.Params.Get("id")
+			if !ok {
+				c.JSON(http.StatusOK, gin.H{"error": "无效的id"})
+				return
+			}
+			var todo Todo
+			if err = DB.Where("id=?", id).First(&todo).Error; err != nil {
+				c.JSON(http.StatusOK, gin.H{"error": err.Error()})
+				return
+			}
+			c.BindJSON(&todo)
+			if err = DB.Save(&todo).Error; err != nil {
+				c.JSON(http.StatusOK, gin.H{"error": err.Error()})
+			} else {
+				c.JSON(http.StatusOK, todo)
+			}
+		})
+		//删除某一待办事项
+		v1Group.DELETE("/todo/:id", func(c *gin.Context) {
+			id, ok := c.Params.Get("id")
+			if !ok {
+				c.JSON(http.StatusOK, gin.H{"error": "无效的id"})
+				return
+			}
+			if err = DB.Where("id=?", id).Delete(Todo{}).Error; err != nil {
+				c.JSON(http.StatusOK, gin.H{"error": err.Error()})
+			} else {
+				c.JSON(http.StatusOK, gin.H{id: "deleted"})
+			}
+		})
+
+	}
+
 	r.Run("localhost:8080")
 }
