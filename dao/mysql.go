eb2ee5196f1ffcde288e1b0b591bd694b7e8d525
chuxin <chuxin0816@qq.com>
2023-08-30 22:06:50 +0800

    improve security

--- dao/mysql.go
+++ dao/mysql.go
@@ -8,7 +8,7 @@ import (
 var DB *gorm.DB
 
 func InitMysql() (err error) {
-	dsn := "root:ljh20040816@tcp(127.0.0.1:3306)/bubble?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:123456@tcp(127.0.0.1:3306)/bubble?charset=utf8mb4&parseTime=True&loc=Local"
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	return
 }
